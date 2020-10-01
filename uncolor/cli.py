244aab8e98d66f049c951ca3649c90c3f38adc72
Ryan Delaney <ryan.patrick.delaney@protonmail.com>
2020-10-01 11:09:40 -0400

    Fix black formatting for new version

--- uncolor/cli.py
+++ uncolor/cli.py
@@ -7,7 +7,8 @@ import argparse
 def parse_args() -> argparse.Namespace:
     """Define an argument parser and return the parsed arguments."""
     parser = argparse.ArgumentParser(
-        prog="uncolor", description="strips ANSI colors from a data stream",
+        prog="uncolor",
+        description="strips ANSI colors from a data stream",
     )
 
     return parser.parse_args()
